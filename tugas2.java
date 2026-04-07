3365966722a0dc04c530cfaecb9247e40bedf2c2
fdhlrsya <your-email@gmail.com>
2026-04-07 17:42:35 +0700

    first commit

---
+++ tugas2.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+public class tugas2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String nama;
+        int usia;
+
+        System.out.print("Input nama : ");
+        nama = input.nextLine();
+
+        System.out.print("Input usia: ");
+        usia = input.nextInt();
+
+        System.out.println(" Nama saya adalah " + nama + " dan usia saya " + usia + " tahun");
+        
+
+    }
+}
---
+++ tugas3.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class tugas3 {
+    public static void main (String[] args) {
+        
+        try (Scanner input = new Scanner(System.in)) {
+            System.out.println("================");
+            System.out.print( "Input Kehadiran :");
+            int kehadiran = input.nextInt();
+            System.out.print("Input Tugas    :");
+            int tugas = input.nextInt();
+            System.out.print( "Input Uts    :");
+            int uts = input.nextInt();
+            System.out.print( "Input Uas    :");
+            int uas = input.nextInt();
+
+            int realisasi = 21;
+            System.out.println( "===============");
+
+            double bKehadiran = ((double) kehadiran / realisasi) * 10 /100;
+            double bTugas = (double) tugas  * 20 /100;
+            double bUts = (double) uts  * 30 /100;
+            double bUas = (double) uas  * 40 /100;
+            double total = bKehadiran + bTugas + bUts + bUas;
+
+            System.out.println("Bobot Kehadiran :" + bKehadiran);
+            System.out.println("Bobot Tugas :" + bTugas);
+            System.out.println("Bobot Uts :" + bUts);
+            System.out.println("Bobot Uas :" + bUas);
+            System.out.println("Total    :" + total);
+        }
+
+
+
+
+
+    }
+}
